3c4a12834d7abcf5f690a228b3f4efe3a732fd8c
backgtx <wesleyafc2010@gmail.com>
2021-08-05 23:06:00 -0300

    create account

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  const userExists = users.some((user) => user.username === username)
+  if (userExists) {
+    return response.status(400).json({ err: "username already exists" })
+  }
+
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todos: []
+  })
+  return response.status(201).json(users).send()
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
